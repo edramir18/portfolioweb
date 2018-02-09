abab44b470e90e5ba0ae05169bd8afc842233ce6
Eduardo Ramirez <edramir18@gmail.com>
2018-02-09 16:00:35 -0400

    Preliminar Version

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,18 @@
 'use strict'
 
+function portfolioHandler (event) {
+  const profile = document.getElementById('profile')
+  profile.classList.add('-vanished')
+  profile.addEventListener('animationend', (event) => {
+    const main = document.querySelector('main')
+    const template = document.getElementById('portfolio').content.cloneNode(true)
+    template.querySelector('.portfolio').classList.add('-is-showed')
+    main.replaceChild(template, event.target)
+  })
+}
+
 document.addEventListener('DOMContentLoaded', (e) => {
   console.log('DOM Loaded')
+  const btn = document.getElementById('btn_portfolio')
+  btn.addEventListener('click', portfolioHandler)
 })
